08c13ee8469772abeb0cfa6690dd84997de03974
rekan <rekanagy06@gmail.com>
2017-03-20 12:22:41 +0100

    Created project and Destination() class

---
+++ src/main/java/Destination.java
@@ -0,0 +1,24 @@
+public class Destination {
+    private String name;
+    private Destination dependency;
+
+    public Destination(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Destination getDependency() {
+        return dependency;
+    }
+
+    public void setDependency(Destination dependency) {
+        this.dependency = dependency;
+    }
+}

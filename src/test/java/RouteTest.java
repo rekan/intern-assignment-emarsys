9a440243006afbcd43d00fce20b707fc4112bd42
rekan <rekanagy06@gmail.com>
2017-03-20 12:29:53 +0100

    Added JUnit maven dependency

---
+++ src/test/java/RouteTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+public class RouteTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void generateRoute() throws Exception {
+
+    }
+
+}
